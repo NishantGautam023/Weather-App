e007d0ce042665ed9fc87915da37afc077cae4a4
Nishant Gautam <eddienishant@gmail.com>
2020-06-12 00:33:23 +0530

    tried tailwind integration

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
-# Create your views here.
-def homePageView(request):
-    return HttpResponse('Hello world!')
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
