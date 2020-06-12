d57d2ee744d70e6f474fa1effcf09c4ec7a5f5c4
Nishant Gautam <eddienishant@gmail.com>
2020-06-12 13:21:16 +0530

    first test of the app completed

--- weather_app/tests.py
+++ weather_app/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+class simpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code,200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code,200)    
+
+#  we perform a check if the status code
+# for each page is 200, which is the standard response for a successful HTTP request.
+# That’s a fancy way of saying it ensures that a given webpage actually exists, but says
+# nothing about the content of said page.
