51faf24575d87334d11586f5fed50f4cfbaf5709
Nishant Gautam <eddienishant@gmail.com>
2020-06-12 12:05:19 +0530

    Basic structure completed

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path # to power our urlPattern we need to import path
-from .views import HomePageView # to ook in the current directory of views.py
+from .views import HomePageView, AboutPageView # to ook in the current directory of views.py
 urlpatterns = [
-    path('',HomePageView.as_view(),name="home")
+    path('',HomePageView.as_view(),name="home"),
+    path('about/',AboutPageView.as_view(),name="about")
 ]
