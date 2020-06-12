5e05708ff8bd10e7c3c22050723efe56b785dc36
Nishant Gautam <eddienishant@gmail.com>
2020-06-12 12:53:01 +0530

    Extending templates completed

--- my_weather/settings.py
+++ my_weather/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'my_weather.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
